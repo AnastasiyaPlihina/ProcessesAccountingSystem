2c27e712b32c859b3c8308da592cebe9d7a09ece
Anastasia <a.a.pligina@gmail.com>
2022-11-30 11:42:20 +0300

    add auditor role

--- src/main/java/by/tms/diploma/entity/Role.java
+++ src/main/java/by/tms/diploma/entity/Role.java
@@ -5,7 +5,8 @@ import org.springframework.security.core.GrantedAuthority;
 public enum Role implements GrantedAuthority {
     ADMIN,
     HEAD_OF_DEPARTMENT,
-    WORKER;
+    WORKER,
+    AUDITOR;
 
     @Override
     public String getAuthority() {
